d282c0951b2e945a49e75d8a3c2f8a5646784563
Francisco-Castro <catrapato@hotmail.com>
2021-04-12 23:55:45 -0400

    User interface

--- lib/rumbl_web/views/user_view.ex
+++ lib/rumbl_web/views/user_view.ex
@@ -10,7 +10,15 @@ defmodule RumblWeb.UserView do
   end
 
   def view_link(conn, %User{id: user_id}) do
-    link("View", to: Routes.user_path(conn, :show, user_id))
+    link("View", to: Routes.user_path(conn, :show, user_id), class: "btn btn-sm btn-primary", style: "margin: 0 0 0 5px")
+  end
+
+  def edit_link(conn, %User{id: user_id}) do
+    link("Edit", to: Routes.user_path(conn, :edit, user_id), class: "btn btn-sm btn-success", style: "margin: 0 0 0 5px")
+  end
+
+  def delete_link(conn, %User{id: user_id}) do
+    link("Delete", to: Routes.user_path(conn, :delete, user_id), method: "delete", class: "btn btn-sm btn-danger", style: "margin: 0 0 0 5px")
   end
 
 end
